7d66b065d4cf31518452a8119143577455ec6b38
Harsha Dama <damasriharsha@gmail.com>
2022-01-06 12:14:21 -0500

    put files into packages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"myapp/pkg/headers"
 	"net/http"
 )
 
@@ -9,8 +10,8 @@ const port string = ":8080"
 
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", headers.Home)
+	http.HandleFunc("/about", headers.About)
 
 	log.Printf("listening on %s", port)
 
