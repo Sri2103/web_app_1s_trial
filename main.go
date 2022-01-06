054159467da44a795bb3ec69041ffd7e8717813f
Harsha Dama <damasriharsha@gmail.com>
2022-01-06 11:38:29 -0500

    Re-organized code and template modification

--- main.go
+++ main.go
@@ -1,33 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-	"text/template"
 )
 
 const port string = ":8080"
-
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.html")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.html")
-
-}
-
-func renderTemplate(w http.ResponseWriter, html string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + html)
-
-	err := parsedTemplate.Execute(w, nil)
-
-	if err != nil {
-		fmt.Println("error in Parsling Template :", err)
-		return
-	}
-}
 
 func main() {
 
